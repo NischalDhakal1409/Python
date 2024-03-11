fd2f83440112ddde78bebae94ce9155d1ccb1760
Nischal <nischaldhakal1409@gmail.com>
2024-03-11 21:16:11 +0545

    Initial Commit

---
+++ stringmethods.py
@@ -0,0 +1,12 @@
+name = "Nischal"
+
+#print(len(name))
+#print(name.find("i"))
+#print(name.capitalize())
+#print(name.upper())
+#print(name.lower())
+#print(name.isalnum())
+#print(name.isalpha())
+#print(name.count("i"))
+#print(name.replace("i","z"))
+#print(name*3)
