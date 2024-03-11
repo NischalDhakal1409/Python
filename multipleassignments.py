e21afe92a728e96338e9e5c0e69ef6c37abf3890
Nischal <nischaldhakal1409@gmail.com>
2024-03-11 20:54:17 +0545

    Init Changes

---
+++ multipleassignments.py
@@ -0,0 +1,7 @@
+#Multiple assignments allows us to assign multiple variables at the same time in one line of code.
+
+name, age, foodie = "Nischal", 21, True
+
+print(name)
+print(age)
+print(foodie)
