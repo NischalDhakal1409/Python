cd1f9a1dd0b4fe1c0752d2dafdb78c6b504b35e7
Nischal <nischaldhakal1409@gmail.com>
2024-03-11 20:49:19 +0545

    Init Changes

--- variables.py
+++ variables.py
@@ -1,5 +1,6 @@
-#String data types
+#Types of Variables
 
+#String data types
 first_name = "Nischal"
 last_name = "Dhakal"
 full_name = first_name + " " + last_name
@@ -7,8 +8,20 @@ print (full_name)
 print(type(first_name))
 
 #Integrer Data type
-
 age = 21
 age += 1
 print(age)
 print(type(age))
+print("Your age is: " + str(age))
+
+#Float Data Types
+height = 55.6
+print(height)
+print(type(height))
+print("Your height is : " + str(height))
+
+#Boolean Data Type
+panda = True
+print(panda)
+print(type(panda))
+print("Is it an animal? : " + str(panda))
