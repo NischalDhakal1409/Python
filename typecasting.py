763429ef6162c80a0bbb7f8c62583d012ef91467
Nischal <nischaldhakal1409@gmail.com>
2024-03-12 22:13:41 +0545

    Initiate Commit

---
+++ typecasting.py
@@ -0,0 +1,12 @@
+#Typecasting is the ability that allows us to convert one type of data into another data type.
+
+x = "Ram"
+y = 22
+z = 5.5
+
+y = str(y)
+z = int(z)
+
+print(x)
+print(y)
+print(z)
