9caba9384162342e8a2c9aa0979e29103f167ec6
Nischal <nischaldhakal1409@gmail.com>
2024-03-13 21:03:07 +0545

    Initial commit

---
+++ math.py
@@ -0,0 +1,16 @@
+import math
+
+x = -2
+pi = 3.14
+a=3
+b=4
+c=1
+
+print(round(pi))
+print(math.ceil(pi))
+print(math.floor(pi))
+print(abs(x))
+print(pow(x,2))
+print(math.sqrt(5))
+print(max(a,b,c))
+print(min(a,b,c))
