0d5c61543e2300df5fb37b8c01aaecb041f93d8f
Nischal <nischaldhakal1409@gmail.com>
2024-03-14 09:37:51 +0545

    Changes

---
+++ strings.py
@@ -0,0 +1,14 @@
+#Displaying string
+print("Hello World!")
+
+#Assigning String to the variable
+name = "Smith"
+print(name)
+
+#Multiline String
+
+random = """Lorem ipsum dolor sit amet,
+        consectetur adipiscing elit,
+        sed do eiusmod tempor incididunt
+        ut labore et dolore magna aliqua."""
+print(random)
