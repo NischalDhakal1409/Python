7d68f9f4b69bc00cf07c31f01fcdf098244dc29e
Nischal <nischaldhakal1409@gmail.com>
2024-03-14 21:44:16 +0545

    Initial Commit

---
+++ Programs/boolean.py
@@ -0,0 +1,16 @@
+#Demonstration of Boolean
+print(10>2)
+
+#Boolean in if-else statement
+a = 10
+b = 20
+
+if b>a:
+    print("b is greater than a")
+
+#Evaluating value
+x = "Hi"
+print(bool(x))
+
+y = 0
+print(bool(y))
