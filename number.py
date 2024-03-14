bde9cc2c2f558487d5ab4a007241c54ebe6a199b
Nischal <nischaldhakal1409@gmail.com>
2024-03-14 09:28:13 +0545

    Changes Comit

---
+++ number.py
@@ -0,0 +1,14 @@
+#Types of Numbers in Python
+
+#Integrer 
+age = 21
+age += 1
+print(age)
+print(type(age))
+print("Your age is: " + str(age))
+
+#Float
+height = 55.6
+print(height)
+print(type(height))
+print("Your height is : " + str(height))
