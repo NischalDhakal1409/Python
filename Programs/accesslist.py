d829dcfe8a9cb9a136ed6dfcab51a33c1584f6ac
Nischal <nischaldhakal1409@gmail.com>
2024-03-14 23:00:08 +0545

    Initial Commit

---
+++ Programs/accesslist.py
@@ -0,0 +1,16 @@
+#Accesing items from positive index
+lang = ["Python","C","Java"]
+print(lang[1])
+
+#Accesing items from negative index
+num = [1,3,5,2,3]
+print(num[-2])
+
+#Accesing from range of index
+random = ["a","c","q","r","i","p","g"]
+print(random[2:5])
+
+#Checking if the items exists or not
+fruits = ["mango", "apple", "grapes"]
+if "apple" in fruits:
+  print("Yes, 'apple' is in the fruits list")
