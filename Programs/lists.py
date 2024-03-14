8e57f36ba84e838e1fde32f89deb192304ca06f0
Nischal <nischaldhakal1409@gmail.com>
2024-03-14 22:27:39 +0545

    Initial Commit

---
+++ Programs/lists.py
@@ -0,0 +1,13 @@
+#General example of list
+name = ["Kevin", "Smith", "Shawn"]
+print(name)
+
+#Demonstration of lists allows duplicate
+lang = ["Eng", "French", "Japanese", "Eng", "French"]
+print(lang)
+
+#Demonstrating that the lists can be of any data type
+numlist = [1,2,3,4]
+randomlist = [ "xyz", True, 22.5, 40 ]
+print(numlist)
+print(randomlist)
