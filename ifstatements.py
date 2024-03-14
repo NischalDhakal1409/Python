0c5ba73b80c3ac1fca749c858a89d49e988aee57
Nischal <nischaldhakal1409@gmail.com>
2024-03-14 08:24:16 +0545

    Initial Commit

--- ifstatements.py
+++ ifstatements.py
@@ -1,8 +1,53 @@
-age = int(input("Enter your age:"))
+a = 20
+b = 30
 
-if age == 70:
-    print("You are old now!")
-elif age>18:
-    print("You are young guy.")
+#Use of If statement
+
+if b > a:
+   print("b is greater than a")
+
+#Use of elif statement
+
+if b > a:
+   print("b is greater than a")
+elif b == a:
+   print("They both are equal")
+
+# Use of else statement
+a=200
+b=20
+
+if b > a:
+   print("b is greater than a")
+elif b == a:
+   print("They both are equal")
 else:
-    print("You are child.")
+   print("a is greater than b")
+
+#Shorthand methods
+   
+if a > b: print("a is greater than b")   # For if statements
+print (a) if a > b else print (b)        # For If-else statements
+
+# Use of Logical Operator in Python
+
+#Use of and
+a = 30
+b = 40
+c = 10
+if a > b and c > a:
+   print("Both conditions are True")
+
+#Use of Or
+a = 200
+b = 33
+c = 500
+if a > b or a > c:
+  print("At least one of the conditions is True") 
+
+
+#Use of Not
+a = 33
+b = 200
+if not a > b:
+  print("a is NOT greater than b")
--- logicaloperators.py
+++
@@ -1,6 +0,0 @@
-temp = int(input("What is the temperature outside today?: "))
-
-if temp >=0 and temp <=38:
-    print("We got good weather today")
-elif temp < 0 or temp > 30:
-    print("We got bad weather today")
