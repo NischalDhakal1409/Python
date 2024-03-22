4da00cee6d5391e07a45e82dc8902805793248f6
Nischal <nischaldhakal1409@gmail.com>
2024-03-22 18:35:12 +0545

    Update Commit

--- Programs/tuples.py
+++ Programs/tuples.py
@@ -1,3 +1,9 @@
 #General example of tuples
 names = ("XYZ", "ZYX", "YZX","XYY")
 print(names)
+
+#Demonstrating that the lists can be of any data type
+numlist = (1,2,3,4)
+randomlist = ("xyz", True, 22.5, 40)
+print(numlist)
+print(randomlist)
