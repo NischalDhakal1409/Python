edf0ac966139d08c954888c03593402f42d02c31
Nischal <nischaldhakal1409@gmail.com>
2024-03-22 18:45:13 +0545

    Initial Commit

---
+++ Programs/managingtuplesitems.py
@@ -0,0 +1,7 @@
+#Accesing items in tuples
+num = (1,2,5,9)
+print(num[1])
+
+#Accessing items in tuples with negeative indexing
+num = (12,346,82,1)
+print(num[-1])
--- Programs/tuples.py
+++ Programs/tuples.py
@@ -2,7 +2,7 @@
 names = ("XYZ", "ZYX", "YZX","XYY")
 print(names)
 
-#Demonstrating that the lists can be of any data type
+#Demonstrating that the lists can be of any data type     
 numlist = (1,2,3,4)
 randomlist = ("xyz", True, 22.5, 40)
 print(numlist)
