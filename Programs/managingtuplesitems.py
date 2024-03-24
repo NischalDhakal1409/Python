70b1fff7758d223f3a8c9435c02e671569a7558f
Nischal <nischaldhakal1409@gmail.com>
2024-03-24 20:18:47 +0545

    Initial Commit

--- Programs/managingtuplesitems.py
+++ Programs/managingtuplesitems.py
@@ -9,3 +9,16 @@ print(num[-1])
 #Accessing range of items 
 alphabet = ("a","ed","as","qo")
 print(alphabet[0:2])
+
+#Adding items in a tuple
+fruits = ("Apple","Banana","Mango")
+y = list(fruits)
+y.append("Pineapple")
+fruits = tuple(y)
+print(fruits)
+
+#Adding tuple to a tuple
+random = (1,2,3,4,67,89,1)
+add = (12,)
+random += add
+print(random)
